55cf8b1fd67d1c95fa6617680b4c9a52cbf5f6be
EvgeniaSimakova <73842206+EvgeniaSimakova@users.noreply.github.com>
2022-04-04 20:05:39 +0300

    Initiated the creation of the project

---
+++ src/by/itstep/nmrsk/javastages/stage12/task01/controller/Main.java
@@ -0,0 +1,23 @@
+package by.itstep.nmrsk.javastages.stage12.task01.controller;
+
+import by.itstep.nmrsk.javastages.stage12.task01.view.Printer;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Printer.print("Input four numbers: ");
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+
+        boolean result = true;
+
+        String msg = result ? "YES" : "NO";
+        Printer.print(msg);
+
+
+    }
+}
